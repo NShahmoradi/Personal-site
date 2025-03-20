31139a6a9468edf22edf53f096418564cbd8d1c9
Narges Shahmoradi <Narcissus.nsh@gmail.com>
2025-03-20 15:21:18 +0330

    Add: User and blog models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=50, unique=True, primary_key=True, blank=False, null=False)
+    email = models.EmailField(max_length=100, unique=True, blank=False, null=False)
+    bio = models.TextField(max_length=100)
+class blog(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50, blank=False, null=False)
+    text = models.TextField()   
+    date = models.DateField(auto_created=True, blank=False, null=False)
+    
+    
